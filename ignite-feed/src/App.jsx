9835c82fc2b4a55efe3f69026d4161eeb063a749
Marcos Junior <mod.gluguer@gmail.com>
2023-07-15 14:28:21 -0300

    inserindo dados; comentarios

--- ignite-feed/src/App.jsx
+++ ignite-feed/src/App.jsx
@@ -5,6 +5,37 @@ import styles from './App.module.css';
 import './global.css';
 import { Sidebar } from './components/Sidebar';
 
+const posts = [
+  {
+    id:1,
+    author: {
+      avatarUrl: 'https://github.com/marcos2junior.png',
+      name: 'Marcos Junior',
+      role: 'Software Developer'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋 '},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 '},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-07-15 12:00:00')
+  },
+  {
+    id:2,
+    author: {
+      avatarUrl: 'https://github.com/diego3g.png',
+      name: 'Diego Fernandes',
+      role: 'CTO @Rocketseat'
+    },
+    content: [
+      { type: 'paragraph', content: 'Fala galeraa 👋 '},
+      { type: 'paragraph', content: 'Acabei de subir mais um projeto no meu portifa. É um projeto que fiz no NLW Return, evento da Rocketseat. O nome do projeto é DoctorCare 🚀 '},
+      { type: 'link', content: 'jane.design/doctorcare'},
+    ],
+    publishedAt: new Date('2023-07-10 12:00:00')
+  }
+];
+
 export function App() {
 
   return (
@@ -14,14 +45,15 @@ export function App() {
       <div className={styles.wrapper}>
         <Sidebar />
         <main>
-          <Post 
-            author="Diego Fernandes" 
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum repellendus at minima ea quos. Consequuntur fuga eligendi saepe! Voluptatem iste nemo, animi ea ipsa similique laborum ad in ex illum!" 
-          />
-          <Post 
-            author="Diego Fernandes" 
-            content="Lorem, ipsum dolor sit amet consectetur adipisicing elit. Earum repellendus at minima ea quos. Consequuntur fuga eligendi saepe! Voluptatem iste nemo, animi ea ipsa similique laborum ad in ex illum!" 
-          />
+         {posts.map(post => {
+          return (
+            <Post key={post.id} 
+              author={post.author} 
+              content={post.content} 
+              publishedAt={post.publishedAt}
+            />
+          )
+         })}
         </main>
       </div>
     </div>
